a454f4ec5110bc50a46822de392073d7046a4fbd
Damian Peralta <damianperaltam@gmail.com>
2021-08-09 17:25:51 +0200

    Adding endpoints to test plain text and json

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const port = process.env.PORT || 4000
 
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/plain-text', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/json', (req, res) => {
+  res.json({msg: "Hello World!"})
 })
 
 app.listen(port, () => {
